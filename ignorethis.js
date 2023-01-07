42e3cb00d7e7c976582082d7e5a3e077786ac2ea
spookyss <81113363+spookysss@users.noreply.github.com>
2023-01-07 11:34:41 -0300

    Update ignorethis.js

--- ignorethis.js
+++ ignorethis.js
@@ -2,3 +2,9 @@ console.log("Hello World!");
 console.log("My name is gustavo, but you can call me sus");
 console.log("I love breaking bad")
 console.log("Random num :" + Math.random());
+
+class Gustavo {
+  send() {
+    console.log("NO YOU N word")
+  }
+}
